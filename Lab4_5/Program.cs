deeaae0055fad2a86da78a041fe768aa35618358
kareemcontreras <66689752+kareemcontreras@users.noreply.github.com>
2020-06-22 23:15:50 -0500

    Add project files.

---
+++ Lab4_5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create the list of scores.
+            List<int> scores = new List<int>();
+            scores.Add(1200);
+            scores.Add(3700);
+            scores.Add(4400);
+            scores.Add(7200);
+            scores.Add(8900);
+
+            //sort the list in default order and print it.
+            scores.Sort();
+
+            foreach (int s in scores)
+            {
+                Console.WriteLine(s);
+
+            }
+
+            //Reverse the list order and print it.
+            scores.Reverse();
+
+            foreach (int s in scores)
+            {
+                Console.WriteLine(s);
+            }
+
+            //Add a new item to the list
+            scores.Add(2400);
+
+            //remove an item from the list
+            scores.Remove(4400);
+        }
+    }
+}
